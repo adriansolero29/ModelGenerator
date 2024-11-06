1283bc4ab8c00c8f803aef2a77d35944866569cf
adriansolero <adrian.manlangit.solero@gmail.com>
2024-11-06 16:30:45 +0800

    Data type converter

---
+++ ModelGenerator.UI/Interface/IDataTypeConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelGenerator.UI.Interface
+{
+    public interface IDataTypeConverter
+    {
+        string Convert(string dbDataType);
+    }
+}
