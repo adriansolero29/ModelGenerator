187a3577faf43d2320c619549f85cdec24725245
adriansolero <adrian.manlangit.solero@gmail.com>
2024-11-10 19:21:23 +0800

    UI changes

--- ModelGenerator.UI/Views/GeneratedModelViewer.Designer.cs
+++ ModelGenerator.UI/Views/GeneratedModelViewer.Designer.cs
@@ -30,44 +30,15 @@ namespace ModelGenerator.UI.Views
         private void InitializeComponent()
         {
             this.TextPanel = new System.Windows.Forms.Panel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.indentFormatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TextPanel
             // 
             this.TextPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextPanel.Location = new System.Drawing.Point(20, 44);
+            this.TextPanel.Location = new System.Drawing.Point(20, 20);
             this.TextPanel.Name = "TextPanel";
-            this.TextPanel.Size = new System.Drawing.Size(478, 497);
+            this.TextPanel.Size = new System.Drawing.Size(478, 521);
             this.TextPanel.TabIndex = 0;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1});
-            this.menuStrip1.Location = new System.Drawing.Point(20, 20);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(478, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.indentFormatToolStripMenuItem});
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
-            this.toolStripMenuItem1.Text = "File";
-            // 
-            // indentFormatToolStripMenuItem
-            // 
-            this.indentFormatToolStripMenuItem.Name = "indentFormatToolStripMenuItem";
-            this.indentFormatToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.indentFormatToolStripMenuItem.Text = "Indent Format";
-            this.indentFormatToolStripMenuItem.Click += new System.EventHandler(this.indentFormatToolStripMenuItem_Click);
             // 
             // GeneratedModelViewer
             // 
@@ -76,24 +47,16 @@ namespace ModelGenerator.UI.Views
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(518, 561);
             this.Controls.Add(this.TextPanel);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "GeneratedModelViewer";
             this.Padding = new System.Windows.Forms.Padding(20);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.GeneratedModelViewer_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel TextPanel;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem indentFormatToolStripMenuItem;
     }
 }
