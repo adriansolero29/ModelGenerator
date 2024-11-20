ccc68db0684fd0308bab60fb6785571d44762f81
adriansolero <adrian.manlangit.solero@gmail.com>
2024-11-20 20:51:15 +0800

    Additional helpers

---
+++ ModelGenerator.UI/Helpers/DatabaseSetting.cs
@@ -0,0 +1,28 @@
+﻿using ModelGenerator.UI.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelGenerator.UI.Helpers
+{
+    public static class DatabaseSetting
+    {
+        public static bool CheckDatabaseSetting()
+        {
+            if (
+                string.IsNullOrEmpty(DataCommunication.ServerName) ||
+                string.IsNullOrEmpty(DataCommunication.PortNumber) ||
+                string.IsNullOrEmpty(DataCommunication.UserId) ||
+                string.IsNullOrEmpty(DataCommunication.Password) ||
+                string.IsNullOrEmpty(DataCommunication.DatabaseName)
+            )
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ModelGenerator.UI/Helpers/DialogHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModelGenerator.UI.Helpers
+{
+    public static class DialogHelpers
+    {
+        public static Form ShowDialog<T>(IServiceProvider provider)
+        {
+            return provider.GetService(typeof(T)) as Form;
+        }
+    }
+}
